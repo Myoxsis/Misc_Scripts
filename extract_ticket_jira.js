0f24a4a9afe44086bb48cc2a4b607c68d3fefa3b
maxime_alain <69240270+Myoxsis@users.noreply.github.com>
2021-11-30 14:30:02 +0100

    Update extract_ticket_jira.js

--- extract_ticket_jira.js
+++ extract_ticket_jira.js
@@ -1,15 +1,28 @@
-for (var i = 0; i < document.querySelectorAll('.actionContainer').length; i++) {
-	acCon = document.querySelectorAll('.actionContainer')[i];
-	dtl = acCon.querySelector('.action-details');
+text = ""
+
+var conts = document.querySelectorAll('.actionContainer');
+
+for (const cont of conts) {
+	dtl = cont.querySelector('.action-details');
 	dt = dtl.querySelector('.date').getAttribute('title');
-	for (var a = 0; a < acCon.querySelectorAll('.changehistory').length; a++) {
-	  text = acCon.querySelectorAll('.changehistory')[a];
-	  act = text.querySelector('.activity-name').innerText.trim();
-	  act1 = text.querySelector('.activity-old-val').childNodes[0].textContent.trim();
-	  act2 = text.querySelector('.activity-new-val').childNodes[0].textContent.trim();
-	  if (act == 'Status'){
-	  	text += act2, "-", dt, "<br>"
-	  	console.log(act2, "-", dt);
-	  } else{}
+	//console.log(cont);
+	//console.log(dt);
+	//await get_activity(cnt);
+
+	var changes = cont.querySelectorAll('.changehistory tr');
+	
+	for (const chg of changes) {
+		//console.log(chg);
+		try{
+			act = chg.querySelector('.activity-name').innerText.trim();
+			act1 = chg.querySelector('.activity-old-val').childNodes[0].textContent.trim();
+			act2 = chg.querySelector('.activity-new-val').childNodes[0].textContent.trim();
+			if (act != 'Status'){
+			} else{
+			var txt = act2 + " - " + dt;
+			text += txt + "\n"
+			}
+		} catch (e){}
 	}
 }
+console.log(text);
